728878e93dc98170e8e946a3f4eb20a7e5dcf47d
Kevin Jose <kevin.josev@gmail.com>
2021-04-11 17:17:54 +0530

    chore(auth-api): configure for heroku

--- index.js
+++ index.js
@@ -8,25 +8,31 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => {
     console.log("Connected to database");
   })
   .catch((err) => {
     console.log("Error connecting to DB", err.message);
   });
+
 app.use(cors());
 app.use(express.json());
 app.use(decodeIDToken);
 
 app.use("/api", usersRouter);
 
-data = {
+const data = {
   msg: "Welcome to bill-keep server",
 };
+
 app.route("/").get((req, res) => res.json(data));
 
-const PORT = process.env.POR || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
