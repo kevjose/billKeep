2c3cbb38d8d652d317b2c2a6fab384448db4c2ca
Kevin Jose <kevin.josev@gmail.com>
2021-04-14 14:02:39 +0530

    feat(user-route) modified post route

--- controllers/users.js
+++ controllers/users.js
@@ -4,19 +4,24 @@ const User = require("../models/user");
 usersRouter.get("/user", async (req, res) => {
   const auth = req.currentUser;
   if (auth) {
-    const user = await User.findOne({});
+    const user = await User.findOne({ phone: auth.phone_number });
     return res.json(user);
   }
   return res.status(403).send("Not authorized");
 });
 
-usersRouter.post("/user", (req, res) => {
+usersRouter.post("/user", async (req, res) => {
   const auth = req.currentUser;
+  console.log(auth, req.body);
   if (auth) {
-    const user = new User(req.body);
-    const savedUser = user.save();
-
-    return res.status(201).json(savedUser);
+    await User.updateOne(
+      { phone: auth.phone },
+      { $set: { name: req.body.name } },
+      { new: true }
+    );
+    return res
+      .status(201)
+      .json({ name: req.body.name, phone: auth.phone_number });
   }
   return res.status(403).send("Not authorized");
 });
